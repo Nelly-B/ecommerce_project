5022f6ed3ecf4310b3a3b4a217f38a6c34e7f4c4
Nelly-B <alapanelly@gmail.com>
2023-08-03 03:43:30 +0100

    first commit

---
+++ core/store/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Category, Product
+
+# Create your views here.
+
+def all_products(request):
+    products = Product.products.all()
+    context ={
+        'products':products
+    }
+    return render(request, 'store/home.html', context)
+
+def product_details(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    context ={
+        'product':product
+    }
+    return render(request, 'store/details.html', context)
+
+def category_list(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    context ={
+        'category':category,
+        'products':products
+    }
+    return render(request, 'store/category.html', context)
